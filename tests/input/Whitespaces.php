e224352abb6bcec16ab448e0203da8200ca7142f
yethee <yethee@users.noreply.github.com>
2022-09-06 17:43:10 +0300

    Add rule SlevomatCodingStandard.Whitespaces.DuplicateSpaces

---
+++ tests/input/Whitespaces.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+class Whitespaces
+{
+    public function assign(): void
+    {
+        $a  = 'foo';
+        $b =  'bar';
+
+        echo 'a = ' . $a;
+        echo 'b = ' . $b;
+    }
+
+    public function match(): void
+    {
+        match (true) {
+            $a   => 'a',
+            $aa  => 'aa',
+            $aaa => 'aaa',
+        };
+    }
+}
