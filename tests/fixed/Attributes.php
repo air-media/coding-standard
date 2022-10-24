c88d08f0ccaca9735ce052dc7981ecec35f914c7
yethee <yethee@users.noreply.github.com>
2022-10-24 11:21:25 +0300

    Add rules for attributes

---
+++ tests/fixed/Attributes.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+/** Comment */
+#[Attribute1]
+#[Attribute2()]
+#[Attribute3('param')]
+class Attributes
+{
+    /**
+     * @psalm-param mixed $param
+     */
+    #[Attribute1]
+    #[Attribute2(option: false)]
+    #[Attribute3()]
+    public function call($param): void
+    {
+    }
+}
---
+++ tests/input/Attributes.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+#[Attribute1, Attribute2()]
+#[Attribute3('param')]
+/** Comment */
+class Attributes
+{
+    #[Attribute1, Attribute2(option: false)]
+    #[Attribute3()]
+    /**
+     * @psalm-param mixed $param
+     */
+    public function call($param): void
+    {
+    }
+}
